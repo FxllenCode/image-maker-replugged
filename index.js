2b2f39cb1e7ea556eb82906401cdb347f49ceeb1
Ethan <ethan@fxllencode.dev>
2022-09-12 14:22:57 -0400

    add no_bitches

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ module.exports = class NoBitches extends Plugin {
     startPlugin () {
         powercord.api.commands.registerCommand({
             command: "no_bitches",
-            description: "Makes a no-bitches image with the following text"
+            description: "Makes a no-bitches image with the following text",
+            usage: "{c} <text>",
+            executor: (args) => ({
+                send: true,
+                result: `https://api.no-bitch.es/${encodeURI(args.join(" "))}`
+            })
         })
     }
+
 }
