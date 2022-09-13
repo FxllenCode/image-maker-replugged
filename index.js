69f0a4e76422067617a6bb83730ed8e0b4bac932
Ethan <ethan@fxllencode.dev>
2022-09-13 15:59:29 -0400

    feat: blahak

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const { Plugin } = require('powercord/entities');
+const http = require('powercord/http');
 
 module.exports = class NoBitches extends Plugin {
-    startPlugin () {
-        powercord.api.commands.registerCommand({
-            command: "no_bitches",
-            description: "Makes a no-bitches image with the following text",
-            usage: "{c} <text>",
-            executor: (args) => ({
-                send: true,
-                result: `https://api.no-bitch.es/${encodeURI(args.join(" "))}`
-            })
-        })
-    }
+  async startPlugin() {
+    powercord.api.commands.registerCommand({
+      command: 'no_bitches',
+      description: 'Makes a no-bitches image with the following text',
+      usage: '{c} <text>',
+      executor: (args) => ({
+        send: true,
+        result: `https://api.no-bitch.es/${encodeURIComponent(args.join(' '))}`,
+      }),
+    });
+    powercord.api.commands.registerCommand({
+      command: 'blahaj',
+      description: 'Finds a blahaj image for you',
+      usage: '{c}',
+      executor: async () => ({
+        send: true,
+        result: await this.findBlahaj(),
+      }),
 
-}
+    });
+  }
+
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('no_bitches');
+    powercord.api.commands.unregisterCommand('blahaj');
+  }
+
+  async findBlahaj() {
+    http.get();
+    const response = await http.get('https://blahaj.shop/api/random/text');
+    const text = response.body.toString();
+    return text;
+  }
+};
